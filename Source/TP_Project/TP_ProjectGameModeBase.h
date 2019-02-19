05d0de54fdf9fdcfd2b7ca7060cb500085c960ff
Tariq Sayed <sayedta91@gmail.com>
2019-02-19 16:51:03 +0000

    Initial commit

---
+++ Source/TP_Project/TP_ProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TP_ProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TP_PROJECT_API ATP_ProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
